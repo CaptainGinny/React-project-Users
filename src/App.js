b809b1f350067e39b1d2dcaffe5997d76d3addaa
CaptainGinny <ginnymalko@gmail.com>
2024-05-04 12:09:42 +0300

    img changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+// GET https://reqres.in/api/users
 
 function App() {
   const [users, setUsers] = React.useState([]);
